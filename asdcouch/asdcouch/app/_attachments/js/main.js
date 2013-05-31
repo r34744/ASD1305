b7c568cb99cb122b413701322110a3d497bfb019
Greg Koenig <gkoenig@fullsail.edu>
2013-05-30 23:23:07 -0400

    end of the line

--- asdcouch/asdcouch/app/_attachments/js/main.js
+++ asdcouch/asdcouch/app/_attachments/js/main.js
@@ -5,125 +5,200 @@
 // use .on() instead of .bind()
 //
 
-
-$('#home').on('pageinit', function(){
+$(document).on('pageinit', '#home', function(){
 	//code needed for home page goes here
 });	
 		
-$('#teams').on('pageinit', function(){
+$(document).on('pageinit', '#races', function(){
      
-     $.ajax({
-        "url": "_view/races",
-        "dataType": "json",
-        "success": function(data) {
-           $.each(data.rows,function(index, team){
-           var teamName = team.value.teamname;
-           var race1 = team.value.Race1;
-           var race2 = team.value.Race2;
-           $("#teamlist").append(
+     $.couch.db("asdteams").view("app/races", {
+        success: function(data) {
+           $("#racelist").empty();
+           $.each(data.rows,function(index, date){
+           var date = (date.key);
+            $("#racelist").append(
            	$("<li>").append(
-           	$("<a>").attr("href", "#Results")
-           		.text(teamName)
+           	$("<a>").attr("href", "pastrace.html?date="+date)
+           		.text(date)
            		)
            	);
        	});
-       	$("#teamlist").listview("refresh");
+       	$("#racelist").listview("refresh");
        	}
        });
-    
-    
-    
-    
+   
 });
 
 
-$('#addTeams').on('pageinit', function(){
+	
+	
+
+
+
+$(document).on('pageinit', '#pastrace', function(){
+		var urlData = $(this).data("url");
+		var urlParts = urlData.split("?");
+		var urlPairs = urlParts[1].split("&");
+		var urlValues = {};
+		for (var pair in urlPairs) {
+			var keyValue = urlPairs[pair].split("=");
+			var key = decodeURIComponent(keyValue[0]);
+			var value= decodeURIComponent(keyValue[1]);
+			urlValues[key] = value;
+		}
+		//console.log(urlValues);
+	
+	var racedate = urlValues["date"];
+	//console.log(racedate);
+	
+	$.couch.db("asdteams").view("app/singlerace", {
+        key: racedate,
+        success: function(data) {
+		$("#pastracecontent").empty();
+		//console.log(data);
+		 $.each(data.rows,function(index, race){
+           var date = (race.key);
+          	$("#pastracecontent").append(
+           		$("<h1>").text(racedate)
+           	);
+          	
+          	$("#pastracecontent").append(
+           		$("<table>").attr("id", "resulttable")
+           	);
+           $("#resulttable").attr("class", "table-stripe");
+           	
+           	$("#resulttable").append(
+           		$("<thead>").append(
+           			$("<tr>").attr("id", "teamtablehead").append(
+           				$("<th>").text("School")
+           				
+           			)
+           		)
+           	);
+            
+            $("#teamtablehead").append(
+           			$("<th>").text("Race #1")
+           	);
+           					
+           	$("#teamtablehead").append(
+           			$("<th>").text("Race #2")
+           	);					
+            
+            
+            $("#resulttable").append(
+           		$("<tr>").attr("id", "team1table").append(
+           			$("<th>").text(race.value.teamname1)
+           		)
+           	);
+            $("#resulttable").append(
+           		$("<tr>").attr("id", "team2table").append(
+           			$("<th>").text(race.value.teamname2)
+           		)
+           	);
+           	$("#resulttable").append(
+           		$("<tr>").attr("id", "team3table").append(
+           			$("<th>").text(race.value.teamname3)
+           		)
+           	);
+           $("#resulttable").append(
+           		$("<tr>").attr("id", "team4table").append(
+           			$("<th>").text(race.value.teamname4)
+           		)
+           	);
+           	$("#resulttable").append(
+           		$("<tr>").attr("id", "team5table").append(
+           			$("<th>").text(race.value.teamname5)
+           		)
+           	);
+           	$("#resulttable").append(
+           		$("<tr>").attr("id", "team6table").append(
+           			$("<th>").text(race.value.teamname6)
+           		)
+           	);
+           	
+          	$("#team1table").append(
+           		$("<td>").text(race.value.team1race1)
+           	);
+           	$("#team1table").append(
+           		$("<td>").text(race.value.team1race2)
+           	);
+           	
+           	$("#team2table").append(
+           		$("<td>").text(race.value.team2race1)
+           	);
+           	$("#team2table").append(
+           		$("<td>").text(race.value.team2race2)
+           	);
+           	
+           	$("#team3table").append(
+           		$("<td>").text(race.value.team3race1)
+           	);
+           	$("#team3table").append(
+           		$("<td>").text(race.value.team3race2)
+           	);
+           	
+           	$("#team4table").append(
+           		$("<td>").text(race.value.team4race1)
+           	);
+           	$("#team4table").append(
+           		$("<td>").text(race.value.team4race2)
+           	);
+           	
+           	$("#team5table").append(
+           		$("<td>").text(race.value.team5race1)
+           	);
+           	$("#team5table").append(
+           		$("<td>").text(race.value.team5race2)
+           	);
+           	
+           	$("#team6table").append(
+           		$("<td>").text(race.value.team6race1)
+           	);
+           	$("#team6table").append(
+           		$("<td>").text(race.value.team6race2)
+           	);
+       	});
+       	//$("#pastracecontent").listview("refresh");
+       	}
+       });
+
+});
+
+
+$(document).on('pageinit', '#newRace', function(){
      
     
     
-    var defaultFillData= function (){
-        $("#addTeams").innerHTML = "";
-        var id = Math.floor(Math.random()*100000000);
-            localStorage.setItem(id, JSON.stringify(json));
-        for (var i=0, j=localStorage.length; i<j; i++) {
-        var key = localStorage.key(i);
-        var value = localStorage.getItem(key);
-        var object = JSON.parse(value);
-        };
-        var Team1 = object[0].value;
-        var Team2 = object[1].value;
-        var Team3 = object[2].value;
-        var Team4 = object[3].value;
-        var Team5 = object[4].value;
-        var Team6 = object[5].value;
-        
-        $('#team1').val(Team1);
-        $('#team2').val(Team2);
-        $('#team3').val(Team3);
-        $('#team4').val(Team4);
-        $('#team5').val(Team5);
-        $('#team6').val(Team6);
     
-    };
-    
-    
-    
-    $("#loaddefault").on('click',defaultFillData);
-    
-    var parseForm = function(data) {
-    console.log(data);   
-    };
     var errorlink = $('#errorlink');
     var savedlink = $('#savedlink');
     var teamNamesForm = $("#teamNamesForm");
     
-    teamNamesForm.validate({
-        invalidHandler: function(form, validator){
-            var html='';
-            for (var key in validator.submitted){
-                var label = $('label[for^="' + key +'"]').not('[generated]');
-                var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-                var fieldName = legend.length ? legend.text() : label.text();
-                html += '<li>' + fieldName + '</li>';
-            };
-           
-        },
-        submitHandler: function(key){
-            var data= teamNamesForm.serializeArray();
-            var html='';
-            
-            if(!key) {
-                var id = Math.floor(Math.random()*100000000);
-                //if same set it as the old.
-                localStorage.setItem(id, JSON.stringify(data));
-            }else{
-                var id = key;
-                localStorage.setItem(id, JSON.stringify(data));
-            }
-            //parseForm(data);
-            alert("Teams are saved");
-            
-        }
+    $('#teamNamesForm').submit(function() {
+  		var formSerial = $(this).serialize();
+  		console.log(formSerial);
+  		var splitForm = formSerial.split("&");
+  		console.log(splitForm);
+		});
         
-    });
-    
-    var clearLocal = function(){
-        localStorage.clear();
-        return false;
-    };
+       
+        
+        
+        
+        
     
     
 });
 
-$('#Race').on('pageinit', function(){
+$(document).on('pageinit', '#Race', function(){
 	$.ajax({
         "url": "_view/races",
         "dataType": "json",
         "success": function(data) {
            $.each(data.rows,function(index, team){
-           var teamName = team.value.teamname;
-           var race1 = team.value.Race1;
-           var race2 = team.value.Race2;
+           var teamName = team.value.teamname1;
+           var race1 = team.value.team1race1;
+           var race2 = team.value.team1race2;
            $("#racebuttons1").append(
            	$("<button>").attr("class", "racebutton").attr("data-role", "button").text(teamName)
            
@@ -136,7 +211,7 @@ $('#Race').on('pageinit', function(){
   
 });
 
-$('#Race2').on('pageinit', function(){
+$(document).on('pageinit', '#Race2', function(){
 	 
     $.ajax({
         "url": "_view/races",
@@ -163,7 +238,7 @@ $('#Race2').on('pageinit', function(){
 });	
 
 
-$('#Results').on('pageinit', function(){
+$(document).on('pageinit', '#Results', function(){
      
      $.ajax({
         "url": "_view/races",
--- asdcouch/asdcouch/app/views/races/map.js
+++ asdcouch/asdcouch/app/views/races/map.js
@@ -1,9 +1,6 @@
-function(doc) {
-  if (doc._id.substr(0,5) === "team:") {
-    emit(doc._id.substr(5), {
-         "teamname": doc.teamname,
-         "Race1": doc.Race1,
-         "Race2": doc.Race2
-    });
-  }
+function (doc) {
+	if (doc._id.substr(0, 5) === "date:"){
+  	emit(doc._id.substr(5));
+  	
+  	}
 };
